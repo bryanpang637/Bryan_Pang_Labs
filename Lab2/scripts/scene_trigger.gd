febf5016cf64a81f75fa8de46180c6a800c12876
bryanpang637 <bryanpangth@gmail.com>
2024-12-16 14:53:58 -0500

    1

---
+++ Lab2/scripts/scene_trigger.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+@export var connected_scene: String
+var scene_folfer = "res://scenes/"
+
+
+func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	pass # Replace with function body.
